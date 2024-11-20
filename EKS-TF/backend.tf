e8c1055d8013e2369f729ce68d3e631e18bb123d
pellurimadhu <39980747+pellurimadhu@users.noreply.github.com>
2024-11-20 11:20:41 +0530

    changed bucket name

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "terraform-youtubebackend" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
